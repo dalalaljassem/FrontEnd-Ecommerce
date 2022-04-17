2fc692b3799e63f022540713ed09f4777b5e09de
dalalaljassem <dalal.aljassem@gmail.com>
2022-04-17 03:54:20 +0300

    added cartUI

--- slider.js
+++ slider.js
@@ -1,7 +1,6 @@
+// making thumbnails move
 let thumbnails = document.getElementsByClassName('thumbnail')
-
 let activeImages = document.getElementsByClassName('active')
-
 for (var i=0; i < thumbnails.length; i++){
 
     thumbnails[i].addEventListener('mouseover', function(){
@@ -15,3 +14,36 @@ for (var i=0; i < thumbnails.length; i++){
         document.getElementById('featured').src = this.src
     })
 }
+
+// cart stuff
+let numberOfProduct = 0;
+  function decrement() {
+    numberOfProduct--;
+    if (numberOfProduct < 0) {
+        let numberOfProduct = 0; }
+    document.getElementById("numberOfProduct").innerHTML = numberOfProduct;
+  }
+  
+  function increment() {
+    numberOfProduct++;
+    document.getElementById("numberOfProduct").innerHTML = numberOfProduct;
+  }
+  
+  function showItems() {
+    var items = document.getElementById("cartUI");
+  
+    if (items.style.display === "none") {
+        items.style.display = "block";
+      showCart = true;
+    } else {
+        items.style.display = "none";
+      showCart = true;
+    }
+  }
+  
+  function addToCart() {
+    if (numberOfProduct !== 0) {
+//add to cart
+      }
+
+  }
